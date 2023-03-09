d5e0841a66b591f9d3a20ba93174ed4e3cf837fc
Tony Pujals <tpujals@gmail.com>
2023-03-08 16:26:52 -0800

    feat: add google identity provider to auth api

---
+++ app/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import assert from 'node:assert';
+
+assert(process.env.GOOGLE_CLIENT_ID);
+assert(process.env.GOOGLE_CLIENT_SECRET);
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
