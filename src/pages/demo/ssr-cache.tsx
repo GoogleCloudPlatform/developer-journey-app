dd84d90e6042d3ec56817c18b761df4995034e31
Tony Pujals <tonypujals@google.com>
2023-03-14 02:45:31 -0700

    demo: ssg and ssr cached pages (#4)

---
+++ src/pages/demo/ssr-cache.tsx
@@ -0,0 +1,28 @@
+import {GetServerSidePropsContext, GetServerSidePropsResult} from "next";
+
+interface PropsData {
+  greeting: string,
+}
+
+// Next.js will call this function for every request
+// (if cache TTL has expired)
+export async function getServerSideProps({res}: GetServerSidePropsContext): Promise<GetServerSidePropsResult<PropsData>> {
+  // Must explicitly set Cache-Control for SSR pages.
+  res!.setHeader(
+      'Cache-Control',
+      'public, s-maxage=10, stale-while-revalidate'
+  );
+
+  const data: PropsData = {
+    greeting: "Hello",
+  }
+
+  return {
+    props: data,
+  };
+}
+
+export default function Page({greeting}: {greeting: string}) {
+  return <div>{greeting}! This page was dynamically <strong>server-side
+    rendered</strong> and is cacheable.</div>;
+}
