358abaca0b9adcc5d260dcafbe8c1ebe158d1fdb
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-15 11:13:19 -0500

    feat: remove logic from mission component

--- src/components/mission.tsx
+++ src/components/mission.tsx
@@ -1,26 +1,9 @@
-import { useEffect, useState } from "react"
 import Image from 'next/image'
 import { Mission } from "src/models/Mission";
-import { Database } from "../lib/database";
-import { FieldValue } from "@google-cloud/firestore";
-import { initializeApp } from "firebase/app";
-import { getFirestore, setDoc, arrayUnion, connectFirestoreEmulator } from "firebase/firestore";
 import styles from 'src/styles/Mission.module.css'
 
-const app = initializeApp({
-  projectId: "birds-of-paradise",
-  // keyFilename: '/path/to/keyfile.json',
-});
-
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-connectFirestoreEmulator(db, 'localhost', 9999);
-
-import { doc, getDoc } from "firebase/firestore";
-
 export default function Component() {
-  const [missions, setMissions] = useState<Mission[]>([
+  const missions: Mission[] = [
     {
       "id": "AsEOyVuuSPdAAf1ZKhSU",
       "title": "Deploy a NextJS App with a Firestore Database",
@@ -45,51 +28,13 @@ export default function Component() {
       ],
       status: 'NOT_STARTED',
     },
-  ]);
-  const [currentMission, setCurrentMission] = useState(missions[0]);
-
-  const docRef = doc(db, "user", "luke");
-
-  const completeMission = (currentMissionId: string) => {
-    setDoc(docRef, {
-      completedMissions: arrayUnion(currentMissionId)
-    }, { merge: true }).then(getMissions);
-    console.log({ currentMissionId })
-  }
-
-  const getMissions = () => {
-    getDoc(docRef).then((response) => {
-      if (response.exists()) {
-        const newUser = response.data();
-        console.log("Document data:", newUser);
-        const missionsWithStatuses = missions.map(mission => ({
-          ...mission,
-          status: newUser.completedMissions.includes(mission.id) ? 'COMPLETE' : 'NOT_STARTED',
-        }))
-        setMissions(missionsWithStatuses);
-        setCurrentMission(missionsWithStatuses[0]);
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    });
-  }
-
-  useEffect(() => {
-    getMissions();
-  }, []);
+  ];
+  const currentMission = missions[0];
 
   return (
     <>
       <section className={styles.content}>
         <h2>Tiles</h2>
-        <div>
-          <button onClick={() => completeMission(currentMission.id)}>
-            Complete
-            {' '}
-            {currentMission.title}
-          </button>
-        </div>
       </section>
       <section className={styles.controls}>
         <h2>Controls</h2>
@@ -135,13 +80,6 @@ export default function Component() {
       </section>
       <footer className={styles.footer}>
         Mission Selector:
-        {missions.map(mission => (
-          <div key={mission.id}>
-            <button onClick={() => setCurrentMission(mission)}>
-              {mission.title}
-            </button>
-          </div>
-        ))}
       </footer>
     </>
   )
