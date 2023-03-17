f5ac8b96e6d14c29d859e0cc6deabcc1231273fb
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-17 09:48:04 -0500

    feat: require authentication to play game

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Developer Journey App</title>
+        <meta name="description" content="Google Cloud Developer Journey App for JavaScript" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
