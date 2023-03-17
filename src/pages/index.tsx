5ca17ffcecc4ae538e3cc392a0a9d9ee0b595ece
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-17 10:32:42 -0500

    Feature/require auth to play (#15)
    * feat: store completed mission in database
    
    * fix: add renaming in new header component
    
    * chore: move redux provider to _app.tsx
    
    * chore: clean user api requests
    
    * remove game-area file
    
    * feat: require authentication to play game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import { useSession, } from "next-auth/react"
 import styles from 'src/styles/Home.module.css'
 
 // Components
@@ -10,23 +10,19 @@ import Inventory from "../components/inventory";
 import Footer from "../components/footer";
 
 export default function Home() {
+  const { status } = useSession();
   return (
-    <>
-      <Head>
-        <title>Developer Journey App</title>
-        <meta name="description" content="Google Cloud Developer Journey App for JavaScript" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.wrapper}>
-        <Header />
-        <PromptPanel />
-        <TileBoard />
-        <GameControls />
-        <Inventory />
-        <Footer />
-      </main>
-    </>
+    <main className={styles.wrapper}>
+      <Header />
+      {status === "authenticated" && (
+        <>
+          <PromptPanel />
+          <TileBoard />
+          <GameControls />
+          <Inventory />
+          <Footer />
+        </>
+      )}
+    </main>
   )
 }
