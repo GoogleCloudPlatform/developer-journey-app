c0e297b3582239ee0a38e53b70a601fe4766aaab
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-17 08:49:35 -0500

    chore: move redux provider to _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
-import { Provider } from 'react-redux'
-import { store } from '../redux/store'
-import GameArea from "../components/game-area";
+import Head from 'next/head'
+import styles from 'src/styles/Home.module.css'
+
+// Components
+import Header from "../components/header";
+import PromptPanel from "../components/prompt-panel";
+import TileBoard from "../components/tile-board";
+import GameControls from "../components/game-controls";
+import Inventory from "../components/inventory";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <Provider store={store}>
-      <GameArea />
-    </Provider>
+    <>
+      <Head>
+        <title>Developer Journey App</title>
+        <meta name="description" content="Google Cloud Developer Journey App for JavaScript" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.wrapper}>
+        <Header />
+        <PromptPanel />
+        <TileBoard />
+        <GameControls />
+        <Inventory />
+        <Footer />
+      </main>
+    </>
   )
 }
