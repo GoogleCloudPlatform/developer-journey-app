ccb12a8e60a936142d8d2ae8f109478caa00278d
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-20 16:33:30 -0500

    fix: hydrate user mission history data from api

--- src/components/mission-history.tsx
+++ src/components/mission-history.tsx
@@ -1,58 +1,70 @@
 import Image from 'next/image'
-import { useAppSelector } from 'src/redux/hooks';
-import { RootState } from 'src/redux/store';
 import styles from 'src/styles/Mission.module.css'
 import { missions } from 'src/initialData.ts/missions'
+import { useGetUserQuery } from 'src/redux/apiSlice';
 
 export default function MissionHistory() {
-  const user = useAppSelector((state: RootState) => state.user)
+  const {
+    data: user,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUserQuery();
 
-  return (
-    <div className={styles.missionHistory}>
-      Completed Missions:
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Technologies</th>
-            <th>Learning Resources</th>
-          </tr>
-        </thead>
-        <tbody>
-          {user.completedMissions.map((missionId, index) => {
-            const mission = missions.find(mission => mission.id === missionId);
-            if (!mission) return null;
-            return (
-              <tr key={`${mission.id}${index}`}>
-                <td>{mission.title}</td>
-                <td>
-                  {mission.technologies.map(technology => (
-                    <Image
-                      key={technology}
-                      src={`./google-cloud-icons/${technology}.svg`}
-                      alt={`icon of ${technology}`}
-                      width='50'
-                      height='50'
-                    />
-                  ))}
-                </td>
-                <td>
-                  {mission.learningResources.map(learningResource => (
-                    <a
-                      key={learningResource.link}
-                      href={learningResource.link}
-                    >
-                      {learningResource.title}
-                    </a>
-                  ))}
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
-  )
+  if (isLoading) {
+    return <div>Loading...</div>
+  } else if (isSuccess) {
+
+    return (
+      <div className={styles.missionHistory}>
+        Completed Missions:
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Technologies</th>
+              <th>Learning Resources</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.completedMissions.map((missionId, index) => {
+              const mission = missions.find(mission => mission.id === missionId);
+              if (!mission) return null;
+              return (
+                <tr key={`${mission.id}${index}`}>
+                  <td>{mission.title}</td>
+                  <td>
+                    {mission.technologies.map(technology => (
+                      <Image
+                        key={technology}
+                        src={`./google-cloud-icons/${technology}.svg`}
+                        alt={`icon of ${technology}`}
+                        width='50'
+                        height='50'
+                      />
+                    ))}
+                  </td>
+                  <td>
+                    {mission.learningResources.map(learningResource => (
+                      <a
+                        key={learningResource.link}
+                        href={learningResource.link}
+                      >
+                        {learningResource.title}
+                      </a>
+                    ))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  } else if (isError) {
+    return <div>{error.toString()}</div>
+  }
 }
 
 
