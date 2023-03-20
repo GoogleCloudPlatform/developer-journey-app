20bb4b99e8058b6f0894033af795c1f73185751e
Ruy Adorno <ruyadorno@google.com>
2023-03-20 16:43:50 -0400

    chore: add lint github action (#22)
    This changeset adds a linting github action, along with support to
    `prettier` and `jest` when running `eslint`.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'src', 'models', 'redux'],
+  },
 };
 
 module.exports = nextConfig;
