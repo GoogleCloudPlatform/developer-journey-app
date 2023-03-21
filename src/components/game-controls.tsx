725df18be0b2d65d1037e3e12ec2f818072a4289
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-21 15:40:23 -0500

    Feature/tailwind (#31)
    * feat: add navbar
    
    * feat: replace header with navbar
    
    * fix: set user state on load
    
    * fix: fix warning about viewport meta tag
    
    * feat: add styling to index page sections
    
    * add styles for history page
    
    * fix: arrow colors
    
    * fix: arrow colors

--- src/components/game-controls.tsx
+++ src/components/game-controls.tsx
@@ -62,12 +62,12 @@ export default function Component() {
   });
 
   return (
-    <section className="bg-slate-800 text-slate-100 rounded-r-xl p-8 col-span-2 space-y-4">
-      <h2>Controls</h2>
-      <section className="grid grid-cols-3 gap-3 w-fit text-slate-100">
+    <section className="bg-slate-800 rounded-r-xl p-8 col-span-2 space-y-4">
+      <h2 className='text-slate-100'>Controls</h2>
+      <section className="grid grid-cols-3 gap-3 w-fit">
         <div />
         <button
-          className='bg-slate-100 text-slate-900 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded'
+          className='text-slate-900 bg-slate-100 hover:bg-slate-200 py-2 px-4 rounded'
           aria-label="Move player up"
           onClick={() => dispatch(moveUp())}
         >
@@ -75,21 +75,21 @@ export default function Component() {
         </button>
         <div />
         <button
-          className='bg-slate-100 text-slate-900 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded'
+          className='text-slate-900 bg-slate-100 hover:bg-slate-200 py-2 px-4 rounded'
           aria-label="Move player left"
           onClick={() => dispatch(moveLeft())}
         >
           <ArrowLeftIcon className="block h-6 w-6" aria-hidden="true" />
         </button>
         <button
-          className='bg-slate-100 text-slate-900 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded'
+          className='text-slate-900 bg-slate-100 hover:bg-slate-200 py-2 px-4 rounded'
           aria-label="Move player down"
           onClick={() => dispatch(moveDown())}
         >
           <ArrowDownIcon className="block h-6 w-6" aria-hidden="true" />
         </button>
         <button
-          className='bg-slate-100 text-slate-900 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded'
+          className='text-slate-900 bg-slate-100 hover:bg-slate-200 py-2 px-4 rounded'
           aria-label="Move player right"
           onClick={() => dispatch(moveRight())}
         >
