47d81b6f64e2cc169bfeaabc6edf7c447cfffb6f
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-21 10:12:59 -0500

    fix: fix warning about viewport meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,21 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
+import Head from 'next/head';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </SessionProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </SessionProvider>
+    </>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content="Google Cloud Developer Journey App for JavaScript" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
