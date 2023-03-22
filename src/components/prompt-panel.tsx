c644368f2964b4879fdc823f99cb220f765628f1
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-22 08:38:29 -0500

    feat: update prompt text

--- src/components/prompt-panel.tsx
+++ src/components/prompt-panel.tsx
@@ -10,11 +10,14 @@ export default function Component() {
     return (
       <section className="bg-slate-100 text-slate-300 rounded-r-xl p-8 dark:bg-slate-800 my-4 col-span-2 space-y-4">
         <p>
-          Huzzah Traveler! You have gathered all the necessary keys and the location of the treasure has been revealed!
-          Hurry before the evil creatures of the forest block you.
+          Huzzah Traveler! You have gathered all the necessary technologies and the treasure has been revealed!
         </p>
         <p>
-          To learn more, check out these learning resources:
+          Take time to explore these learning resources to
+          {' '}
+          <span className='font-bold text-lg text-slate-100'>{currentMission.title}</span>
+          {' '}
+          before moving on to the next mission:
         </p>
         <ul className='list-disc font-bold text-lg text-slate-100'>
           {currentMission.learningResources.map((learningResource: LearningResource) => (
@@ -25,6 +28,13 @@ export default function Component() {
             </li>
           ))}
         </ul>
+        <p>
+          To see learning resources from all of your completed missions, visit your
+          {' '}
+          <a href='/mission-history' className="font-bold text-lg text-slate-100 underline hover:text-blue-200">
+            Mission History
+          </a>
+        </p>
       </section>
     )
   }
@@ -32,22 +42,27 @@ export default function Component() {
   return (
     <section className="bg-slate-100 text-slate-300 rounded-r-xl p-8 dark:bg-slate-800 my-4 col-span-2 space-y-4">
       <p>
-        Hello traveler, you have just entered the forest of Google Cloud.
-        To unlock complete the mission, you must gather the technologies used to
+        Hello Traveler, you have started your journey in exploring Google Cloud.
+        Your environment holds valuable technoligies that you must gather to complete your mission.
+        To complete the mission, you must gather the technologies used to
         {' '}
         <span className='font-bold text-lg text-slate-100'>{currentMission.title}</span>
         .
       </p>
       <p className='block sm:hidden font-bold text-lg text-slate-100'>
-        To move, click a square adjacent to you.
+        Click a tile adjacent to you to explore your environment.
       </p>
-      <p className='hidden sm:block font-bold text-lg text-slate-100'>
-        To move, use the arrow keys.
+      <p className='hidden sm:block'>
+        Use the
+        {' '}
+        <span className='font-bold text-lg text-slate-100'>Arrow Keys</span>
+        {' '}
+        to explore your environment.
       </p>
       <p>
         Good luck,
         <br />
-        GCP Elders
+        Senior Guides
       </p>
     </section>
 
