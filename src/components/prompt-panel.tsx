9b4348537ad9f7a5cc9c30f1c581d8b79cc09cb8
Jennifer Davis <iennae@gmail.com>
2023-03-21 21:18:54 -0700

    Minor Update to the mission text

--- src/components/prompt-panel.tsx
+++ src/components/prompt-panel.tsx
@@ -11,7 +11,7 @@ export default function Component() {
       <section className="bg-slate-100 text-slate-100 rounded-r-xl p-8 dark:bg-slate-800 my-4 col-span-2 space-y-4">
         <p>
           Huzzah Traveler! You have gathered all the necessary keys and the location of the treasure has been revealed!
-          Hurry before the evil creatures of the forest block you.
+          Hurry before scope creep changes your environment and your mission.
         </p>
         <p>
           To learn more, check out these learning resources:
@@ -32,20 +32,20 @@ export default function Component() {
   return (
     <section className="bg-slate-100 text-slate-100 rounded-r-xl p-8 dark:bg-slate-800 my-4 col-span-2 space-y-4">
       <p>
-        Hello traveler, you have just entered the unknown forest of Google Cloud.
-        The forest holds valuable keys that you must gather to complete your mission.
+        Hello Traveler, you have just started your journey in exploring Google Cloud.
+        Your environment holds valuable keys that you must gather to complete your mission.
         To unlock the final treasure chest, you must gather the technologies used to
         {' '}
         {currentMission.title}.
       </p>
       <p>
-        To explore the forest maze, you can navigate through using W (up), A (left), S (down), D (right) keys -
+        To explore your environment, you can navigate through using W (up), A (left), S (down), D (right) keys -
         and pick up the technologies by hitting Enter.
       </p>
       <p>
         Good luck,
         <br />
-        GCP Elders
+        Senior Guides
       </p>
     </section>
 
