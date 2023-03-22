1a657dbfaf7375c2a94f1ee7f84e45a188df4594
Jennifer Davis <iennae@gmail.com>
2023-03-21 21:19:59 -0700

    minor updates to text

--- src/components/prompt-panel.tsx
+++ src/components/prompt-panel.tsx
@@ -32,7 +32,7 @@ export default function Component() {
   return (
     <section className="bg-slate-100 text-slate-100 rounded-r-xl p-8 dark:bg-slate-800 my-4 col-span-2 space-y-4">
       <p>
-        Hello Traveler, you have just started your journey in exploring Google Cloud.
+        Hello Traveler, you have started your journey in exploring Google Cloud.
         Your environment holds valuable keys that you must gather to complete your mission.
         To unlock the final treasure chest, you must gather the technologies used to
         {' '}
