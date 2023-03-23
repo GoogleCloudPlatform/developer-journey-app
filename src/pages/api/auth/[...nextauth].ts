8ac1aa217b0fc5a46c5d606b7d7dfb1b792f034f
Tony Pujals <tonypujals@google.com>
2023-03-23 11:08:41 -0700

    feat: simplify auth using credentials instead of oauth provider (#38)
    * feat: simplify auth using credentials instead of oauth provider
    
    * update comment

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,36 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import assert from 'node:assert';
+import NextAuth, {AuthOptions} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-assert(process.env.GOOGLE_CLIENT_ID);
-assert(process.env.GOOGLE_CLIENT_SECRET);
 
 export const authOptions: AuthOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    CredentialsProvider({
+      credentials: {
+        username: {label: "Username", type: "text", placeholder: "username"},
+        email: {label: "Email", type: "text", placeholder: "email"},
+        // password: {label: "Password", type: "password"}
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          // Display an  error will be displayed advising the user to check
+          // their details.
+          return null;
+        }
+
+        const username = credentials.username;
+        const email = credentials.email;
+        const user = {id: "1", name: username, email: email};
+
+        if (user) {
+          return user;
+        } else {
+          // Display an  error will be displayed advising the user to check
+          // their details.
+          return null;
+          // Or reject this callback with an Error to send the user to the error
+          // page with the error message as a query parameter
+        }
+      }
     }),
   ],
 };
