5611ceb99c6e88808162f373bda9eb96ad22bf1e
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-24 14:47:01 -0500

    fix: fix button selector for e2e tests (#45)

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -8,7 +8,7 @@ setup('authenticate', async ({ page }) => {
   await page.goto('http://localhost:3000/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F');
   const emailInput = await page.getByRole('textbox', { name: /Username/ })
   await emailInput.fill('CI User');
-  const signInButton = await page.getByRole('button', { type: 'submit' })
+  const signInButton = await page.getByRole('button', { name: 'Sign in with Credentials' })
   await signInButton.click();
   await page.context().storageState({ path: authFile });
 });
