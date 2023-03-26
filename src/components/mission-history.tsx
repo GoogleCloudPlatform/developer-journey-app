0c89813ac46b7820b48cf3c3c456322d0c982fd6
Luke Schlangen <lukeschlangen@gmail.com>
2023-03-25 20:18:33 -0500

    feature: add more diverse missions

--- src/components/mission-history.tsx
+++ src/components/mission-history.tsx
@@ -18,7 +18,11 @@ export default function MissionHistory() {
       <section className="bg-slate-100 text-slate-100 rounded-r-xl p-8 bg-slate-800 my-4 col-span-11 space-y-4">
         <div className="relative overflow-x-auto">
           {isLoading ? ('Loading...') : (<>
-            <div className='mb-2'>{user.completedMissions.length} Completed Missions</div>
+            <div className='mb-2'>
+              {user.completedMissions.filter(missionId => missions.find(mission => mission.id === missionId)).length}
+              {' '}
+              Completed Missions
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-fit">
               {user.completedMissions.map((missionId, index) => {
                 const mission = missions.find(mission => mission.id === missionId);
