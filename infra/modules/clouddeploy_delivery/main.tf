d4719b26119eade59eab5721873dcc62f737a05c
rogerthatdev <roger.that.dev@gmail.com>
2023-04-03 13:51:58 -0700

    add service account resources for clouddeploy

--- infra/modules/clouddeploy_delivery/main.tf
+++ infra/modules/clouddeploy_delivery/main.tf
@@ -1,3 +1,13 @@
+data "google_cloud_run_service" "default" {
+  name     = var.run_service_name
+  project  = var.project_id
+  location = var.region
+}
+
+data "google_service_account" "cloud_run" {
+  account_id = "${data.google_cloud_run_service.default.template[0].spec[0].service_account_name}"
+}
+
 resource "google_clouddeploy_delivery_pipeline" "default" {
   project  = var.project_id
   location = var.region
@@ -16,3 +26,30 @@ resource "google_clouddeploy_delivery_pipeline" "default" {
   }
 }
 
+resource "google_service_account" "default" {
+  project      = var.project_id
+  account_id   = "${var.run_service_name}-cloud-deploy"
+  display_name = "Service Account for Cloud Deploy deployment to Cloud Run."
+}
+
+resource "google_project_iam_member" "deploy_job_runner" {
+  project = var.project_id
+  role    = "roles/clouddeploy.jobRunner"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+resource "google_project_iam_member" "deploy_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+
+resource "google_service_account_iam_binding" "deploy_sa_user_run" {
+  service_account_id = data.google_service_account.cloud_run.id
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [
+    "serviceAccount:${google_service_account.default.email}",
+  ]
+}
