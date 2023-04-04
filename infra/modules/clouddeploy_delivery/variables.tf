45f063d3af4988448083364415e73609bdaa86dc
rogerthatdev <roger.that.dev@gmail.com>
2023-04-03 18:03:05 -0700

    module variables

--- infra/modules/clouddeploy_delivery/variables.tf
+++ infra/modules/clouddeploy_delivery/variables.tf
@@ -31,8 +31,3 @@ variable "run_service_name" {
   type        = string
   description = "The name of the Cloud Run service that this pipeline will deploy to."
 }
-
-# variable "github_repository_url" {
-#   type        = string
-#   description = "URL of connected GitHub repository (https://github.com/repo_owner/repo_name)"
-# }
