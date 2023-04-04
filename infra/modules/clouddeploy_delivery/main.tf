032124021c9ab78aeb9d7d2ccc0248c3c55ff72f
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2023-04-04 14:40:17 -0700

    feat: add Cloud Deploy delivery pipeline (#60)
    * initial terraform files and directory tree
    
    * dev env tf files
    
    * cicd module vars
    
    * terraform for build triggers
    
    * deploy cloudbuild config
    
    * consolidate github repo vars
    
    * throw in a cloud build service account
    
    * add required logging for service account use
    
    * fmt
    
    * add iam roles to build service account
    
    * more builder permissions
    
    * add run service account vars
    
    * update region value
    
    * tf fmt
    
    * new line
    
    * more readme instructions
    
    * add prod files
    
    * replace service account var with data block
    
    * clouddeploy_delivery pipeline resource
    
    * add service account resources for clouddeploy
    
    * add a clouddeploy stage target
    
    * add prod target
    
    * add targets to pipeline
    
    * tf fmt
    
    * add module to dev main.tf
    
    * module variables
    
    * remove comments
    
    * remove cicd module
    
    * remove more cicd files
    
    * remove build yaml

---
+++ infra/modules/clouddeploy_delivery/main.tf
@@ -0,0 +1,98 @@
+data "google_cloud_run_service" "default" {
+  name     = var.run_service_name
+  project  = var.project_id
+  location = var.region
+}
+
+data "google_service_account" "cloud_run" {
+  account_id = data.google_cloud_run_service.default.template[0].spec[0].service_account_name
+}
+
+resource "google_clouddeploy_delivery_pipeline" "default" {
+  project     = var.project_id
+  location    = var.region
+  name        = "${var.deployment_name}-delivery"
+  description = "Basic delivery pipeline for ${var.deployment_name} app."
+  labels      = var.labels
+  serial_pipeline {
+    stages {
+      profiles  = ["stage"]
+      target_id = google_clouddeploy_target.stage.name
+    }
+    stages {
+      profiles  = ["prod"]
+      target_id = google_clouddeploy_target.prod.name
+    }
+  }
+}
+
+resource "google_service_account" "default" {
+  project      = var.project_id
+  account_id   = "${var.run_service_name}-cloud-deploy"
+  display_name = "Service Account for Cloud Deploy deployment to Cloud Run."
+}
+
+resource "google_project_iam_member" "deploy_job_runner" {
+  project = var.project_id
+  role    = "roles/clouddeploy.jobRunner"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+resource "google_project_iam_member" "deploy_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+
+resource "google_service_account_iam_binding" "deploy_sa_user_run" {
+  service_account_id = data.google_service_account.cloud_run.id
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [
+    "serviceAccount:${google_service_account.default.email}",
+  ]
+}
+
+resource "google_clouddeploy_target" "stage" {
+  project     = var.project_id
+  provider    = google-beta
+  location    = var.region
+  name        = "${var.deployment_name}-stage-target"
+  description = "Stage target for ${var.deployment_name} app."
+
+  execution_configs {
+    usages          = ["RENDER", "DEPLOY", "VERIFY"]
+    service_account = google_service_account.default.email
+  }
+
+  labels           = var.labels
+  require_approval = false
+
+  run {
+    location = "projects/${var.project_id}/locations/${data.google_cloud_run_service.default.location}"
+  }
+
+}
+
+resource "google_clouddeploy_target" "prod" {
+  project     = var.project_id
+  provider    = google-beta
+  location    = var.region
+  name        = "${var.deployment_name}-prod-target"
+  description = "Prod target for ${var.deployment_name} app."
+
+  execution_configs {
+    usages          = ["RENDER", "DEPLOY", "VERIFY"]
+    service_account = google_service_account.default.email
+  }
+
+  labels           = var.labels
+  require_approval = false
+
+  run {
+    location = "projects/${var.project_id}/locations/${data.google_cloud_run_service.default.location}"
+  }
+
+}
+
