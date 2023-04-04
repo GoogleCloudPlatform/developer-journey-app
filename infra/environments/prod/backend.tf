ed8b32cacb47ab2b0ce973526423d47364edfd82
rogerthatdev <roger.that.dev@gmail.com>
2023-04-03 18:17:50 -0700

    remove more cicd files

--- infra/environments/prod/backend.tf
+++
@@ -1,6 +0,0 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = ""
-#     prefix = "value"
-#   }
-# }
--- infra/environments/prod/main.tf
+++
@@ -1,7 +0,0 @@
-module "cloud_build_cicd" {
-  source                = "../../modules/cicd"
-  project_id            = var.project_id
-  run_service_name      = var.run_service_name
-  github_repository_url = var.github_repository_url
-  run_service_account   = var.run_service_account
-}
