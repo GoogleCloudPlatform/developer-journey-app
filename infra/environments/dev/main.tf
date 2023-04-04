6b90f1edb257ffed030958b82f510bd4fe82e456
rogerthatdev <roger.that.dev@gmail.com>
2023-04-03 18:02:19 -0700

    add module to dev main.tf

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -4,3 +4,9 @@ module "cloud_build_cicd" {
   run_service_name      = var.run_service_name
   github_repository_url = var.github_repository_url
 }
+
+module "cloud_deploy_delivery" {
+  source                = "../../modules/clouddeploy_delivery"
+  project_id            = var.project_id
+  run_service_name      = var.run_service_name
+}
