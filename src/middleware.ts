64d0fa6f6f5e04778df7d5050b6d3f9a4abbfd26
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2023-06-07 14:28:44 -0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 export {default} from "next-auth/middleware"
 
 export const config = { matcher: ["/demo/page-secured-client-side"] }
