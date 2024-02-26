bcb4bd1e9184bfb40bd0f53c1672226aff505cd0
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2024-02-25 18:47:13 -0800

    remove unused directories from tailwind config (#106)
    Co-authored-by: Katie McLaughlin <katie@glasnt.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
